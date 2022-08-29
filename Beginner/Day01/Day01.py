ab37bbc8422b5d8877d178b3b3f727943d373e69
victorog17 <victor.og17@gmail.com>
2022-08-29 14:37:32 -0300

    commit 20220829

--- Beginner/Day01/Day01.py
+++ Beginner/Day01/Day01.py
@@ -1,4 +1,5 @@
+# Gerador de nome de banda
 print("Bem vindo ao gerador de nome de banda")
 cidade = input(str("Insira o nome da sua cidade natal.\n"))
 pet = input(str("Insira o nome do seu animal de estimação.\n"))
-print(f"O nome da sua banda pode ser {pet} de {cidade}")
+print(f"O nome da sua banda pode ser {pet} de {cidade}.")
